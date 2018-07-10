c0bd6e6d3d11b57ff71ca9c75bd641c165dd79ad
CHGX <52417255@qq.com>
2018-07-10 16:19:06 +0800

    day1

--- app/src/main/java/com/example/administrator/chgx/MainActivity.java
+++ app/src/main/java/com/example/administrator/chgx/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.administrator.chgx;
 
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String[] data={"1","2","3"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //透明状态栏 透明导航栏
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //调整cat亮度
+
+        //test
+        ArrayAdapter<String> adpter =new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,data);
+        ListView listView =(ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adpter);
+
+
+    }
+
+    //改变图片亮度
+    private void changeLight(ImageView imageView, int brightness) {
+        ColorMatrix cMatrix = new ColorMatrix();
+        cMatrix.set(new float[] { 1, 0, 0, 0, brightness, 0, 1, 0, 0, brightness, // 改变亮度
+                0, 0, 1, 0, brightness, 0, 0, 0, 1, 0 });
+        imageView.setColorFilter(new ColorMatrixColorFilter(cMatrix));
     }
 }

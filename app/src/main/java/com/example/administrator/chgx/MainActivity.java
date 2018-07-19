9ee74c94b4b7ab01618bac3b058d2490e7358f21
CHGX <524617255@qq.com>
2018-07-19 10:16:39 +0800

    day10

--- app/src/main/java/com/example/administrator/chgx/MainActivity.java
+++ app/src/main/java/com/example/administrator/chgx/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.administrator.chgx;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -17,6 +21,12 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,12 +35,38 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private List<Fruit> noteList = new ArrayList<>();
     private MyDatabaseHelper dbHelper;
+    public LocationClient mLocationClient;
+    private TextView positionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
         setContentView(R.layout.activity_main);
-        dbHelper = new MyDatabaseHelper(this,"Note.db",null,1);
+
+        //baidu 定位
+        positionText = (TextView) findViewById(R.id.position_text_view);
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!permissionList.isEmpty()) {
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+        } else {
+            requestLocation();
+        }
+
+        //dbHelper
+        dbHelper = new MyDatabaseHelper(this, "Note.db", null, 1);
+
         //RecyclerView
         initNotes();
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycer_view);
@@ -50,19 +86,52 @@ public class MainActivity extends AppCompatActivity {
         }
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setLogo(R.mipmap.logo2);
+    }
 
-     /* //透明状态栏 透明导航栏
-        if (Build.VERSION.SDK_INT >= 21) {
-            View decorView = getWindow().getDecorView();
-            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-            decorView.setSystemUiVisibility(option);
-            getWindow().setNavigationBarColor(Color.TRANSPARENT);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+    //baidu 定位
+    private void requestLocation() {
+        mLocationClient.start();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        {
+            switch (requestCode) {
+                case 1:
+                    if (grantResults.length > 0) {
+                        for (int result : grantResults) {
+                            if (result != PackageManager.PERMISSION_GRANTED) {
+                                Toast.makeText(this, "请同意定位权限！", Toast.LENGTH_SHORT).show();
+                                finish();
+                                return;
+                            }
+                        }
+                        requestLocation();
+                    } else {
+                        Toast.makeText(this, "未知错误", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                    break;
+                default:
+            }
         }
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();*/
+    }
+
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            StringBuilder currentPosition = new StringBuilder();
+            currentPosition.append("纬度：").append(location.getLatitude()).append(" ");
+            currentPosition.append("经线：").append(location.getLongitude()).append(" ");
+            currentPosition.append("定位方式：");
+            if (location.getLocType() == BDLocation.TypeGpsLocation) {
+                currentPosition.append("GPS");
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                currentPosition.append("网络");
+            }
+            positionText.setText(currentPosition);
+        }
     }
 
 
@@ -97,38 +166,40 @@ public class MainActivity extends AppCompatActivity {
         imageView.setColorFilter(new ColorMatrixColorFilter(cMatrix));
     }
 
+
+    //List初始化
     private void initNotes() {
         for (int i = 0; i < 2; i++) {
-            Fruit apple = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1,"CHGX · 10:48");
+            Fruit apple = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1, "CHGX · 10:48");
             noteList.add(apple);
-            Fruit banana = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1,"CHGX · 10:48");
+            Fruit banana = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1, "CHGX · 10:48");
             noteList.add(banana);
-            Fruit orange = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1,"CHGX · 10:48");
+            Fruit orange = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1, "CHGX · 10:48");
             noteList.add(orange);
-            Fruit watermelon = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1,"CHGX · 10:48");
+            Fruit watermelon = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1, "CHGX · 10:48");
             noteList.add(watermelon);
-            Fruit pear = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1,"CHGX · 10:48");
+            Fruit pear = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1, "CHGX · 10:48");
             noteList.add(pear);
-            Fruit grape = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1,"CHGX · 10:48");
+            Fruit grape = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1, "CHGX · 10:48");
             noteList.add(grape);
-            Fruit pineapple = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1,"CHGX · 10:48");
+            Fruit pineapple = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1, "CHGX · 10:48");
             noteList.add(pineapple);
-            Fruit strawberry = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1,"CHGX · 10:48");
+            Fruit strawberry = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1, "CHGX · 10:48");
             noteList.add(strawberry);
-            Fruit cherry = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1,"CHGX · 10:48");
+            Fruit cherry = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1, "CHGX · 10:48");
             noteList.add(cherry);
-            Fruit mango = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1,"CHGX · 10:48");
+            Fruit mango = new Fruit("Prabhdeep listened to this.", R.mipmap.mon1, "CHGX · 10:48");
             noteList.add(mango);
         }
-        SQLiteDatabase db =dbHelper.getWritableDatabase();
-        Cursor cursor = db.query("Note",null,null,null,null,null,null);
-        if(cursor.moveToFirst()){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Cursor cursor = db.query("Note", null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
             do {
-                String name =cursor.getString(cursor.getColumnIndex("name"));
-                Log.d("MainActivity","name="+name);
-                Fruit note1 = new Fruit(name, R.mipmap.mon1,"CHGX · 10:48");
+                String name = cursor.getString(cursor.getColumnIndex("name"));
+                Log.d("MainActivity", "name=" + name);
+                Fruit note1 = new Fruit(name, R.mipmap.mon1, "CHGX · 10:48");
                 noteList.add(note1);
-            }while(cursor.moveToNext());
+            } while (cursor.moveToNext());
         }
         cursor.close();
     }
